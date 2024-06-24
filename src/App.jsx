ccc32b2069e5ed2269af4b2601866bbed81836a8
filippo cafaro <144951463+FilippoDibe@users.noreply.github.com>
2024-06-24 14:48:58 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { BrowserRouter, BrowserRouter as Router } from 'react-router-dom';
+import Main from './components/main/Main';
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Main />
+        </BrowserRouter>
+    );
+}
+
+export default App;
